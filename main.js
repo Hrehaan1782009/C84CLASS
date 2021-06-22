d0a6063c0c3344fc5efeec84a203a70a0676c8ac
Hrehaan1782009 <75216295+Hrehaan1782009@users.noreply.github.com>
2021-06-22 13:58:13 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+background_image = "mars.jpg";
+
+function add() {
+    background_imgTag = new Image();
+    background_img.onload = upload.Background;
+    background_imgTag.src = rover_image;
+}
+
+function uploadBackground() {
+    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadrover() {
+    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+}
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed == '38')
+    {
+        up();
+        console.log("up");
+    }
+    if(keyPressed == '40')
+    {
+        down();
+        console.log("down");
+    }
+    if(keyPressed == '37')
+    {
+        left();
+        console.log("left");
+    }
+    if(keyPressed == '39')
+    {
+        right();
+        console.log("right");
+    }
+}
